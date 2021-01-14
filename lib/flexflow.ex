fbb97a6c3715dce2ce68618255e0ec25878a3e21
clszzyh <clszzyh@gmail.com>
2021-01-14 09:33:05 +0800

    Initial commit
    Signed-off-by: clszzyh <clszzyh@gmail.com>

---
+++ lib/flexflow.ex
@@ -0,0 +1,18 @@
+defmodule Flexflow do
+  @moduledoc """
+  Documentation for `Flexflow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flexflow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flexflow_test.exs
@@ -0,0 +1,8 @@
+defmodule FlexflowTest do
+  use ExUnit.Case
+  doctest Flexflow
+
+  test "greets the world" do
+    assert Flexflow.hello() == :world
+  end
+end
